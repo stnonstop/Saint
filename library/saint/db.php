feb038ddf1e509274248dc542a47c0ceb94d609f
Aziz DURMAZ <stnonstop@gmail.com>
2012-12-21 17:57:33 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	application/models/elementGame.php

---
+++ library/saint/db.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: nevriyedurmaz
+ * Date: 20.12.2012
+ * Time: 21:20
+ * To change this template use File | Settings | File Templates.
+ */
+namespace saint;
+class db
+{
+    public function __construct($dbName){
+
+    }
+}
